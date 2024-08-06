013a584cb91451fe9914fe969cc499d291b52869
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-08-06 16:43:07 +0530

    initialize vite n login

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import './App.css'
+import Chat from "./pages/Chat";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Chat/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
