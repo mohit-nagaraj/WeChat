baddbc9b424e88457ef1c5a7da695caabe83c3a5
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-08-06 22:47:38 +0530

    navbar n registration

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,7 +1,16 @@
+import Navbar from "../components/Navbar";
+
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <Navbar />
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          Chat
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Chat
+export default Chat;
