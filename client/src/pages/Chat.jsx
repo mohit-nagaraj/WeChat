d83fa53219a8530cdf2ed9c59275901d3b4e170e
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-08-11 22:51:54 +0530

    chat ui

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { useEffect, useState, useContext, useCallback } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
@@ -8,15 +9,15 @@ const Chat = () => {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const [userChat, setUserChat] = useState([]);
+  const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchUserChat = useCallback(() => {
     setIsLoading(true);
     axiosInstance
-      .get(`/chats/${user.id}`)
+      .get(`/users/`)
       .then((res) => {
-        setUserChat(res.data);
+        setUsers(res.data);
       })
       .catch((err) => {
         console.log(err);
@@ -38,29 +39,109 @@ const Chat = () => {
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
         <Navbar />
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Chat
-              </h1>
-              {isLoading ? (
-                <p>Loading...</p>
-              ) : (
-                <div className="space-y-4 md:space-y-6">
-                  {userChat?.map((chat) => (
-                    <div key={chat._id} className="flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
-                      <div>
-                        <h2 className="text-lg font-semibold text-gray-900 dark:text-white">{chat.message}</h2>
-                        <p className="text-sm text-gray-500 dark:text-gray-400">{chat.createdAt}</p>
-                      </div>
-                      <div>
-                        <button className="text-sm text-white bg-red-500 px-2 py-1 rounded-lg">Delete</button>
-                      </div>
+        <div class="h-screen shadow-lg rounded-lg">
+          <div class="pt-16 h-full flex flex-row justify-between bg-white">
+            <div class="flex flex-col w-2/5 border-r-2 overflow-y-auto">
+              <div class="border-b-2 py-4 px-2">
+                <input
+                  type="text"
+                  placeholder="search chatting"
+                  class="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
+                />
+              </div>
+
+              {users.map((chat, index) => {
+                return (
+                  <div key={index} class="flex flex-row py-4 px-2 items-center border-b-2">
+                    <div class="w-1/4">
+                      <img
+                        src="https://picsum.photos/200"
+                        class="object-cover h-12 w-12 rounded-full"
+                        alt=""
+                      />
                     </div>
-                  ))}
+                    <div class="w-full">
+                      <div class="text-lg font-semibold">{chat.name}</div>
+                      <span class="text-gray-500">{chat.email}</span>
+                    </div>
+                  </div>
+                );
+              })}
+              {/* <div class="flex flex-row py-4 px-2 items-center border-b-2 border-l-4 border-blue-400">
+                <div class="w-1/4">
+                  <img
+                    src="https://source.unsplash.com/L2cxSuKWbpo/600x600"
+                    class="object-cover h-12 w-12 rounded-full"
+                    alt=""
+                  />
                 </div>
-              )}
+                <div class="w-full">
+                  <div class="text-lg font-semibold">MERN Stack</div>
+                  <span class="text-gray-500">Lusi : Thanks Everyone</span>
+                </div>
+              </div> */}
+            </div>
+
+            <div class="w-full px-5 flex flex-col justify-between">
+              <div class="flex flex-col mt-5">
+                <div class="flex justify-end mb-4">
+                  <div class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+                    Welcome to group everyone !
+                  </div>
+                  
+                </div>
+                <div class="flex justify-start mb-4">
+                  
+                  <div class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quaerat at praesentium, aut ullam delectus odio error sit
+                    rem. Architecto nulla doloribus laborum illo rem enim dolor
+                    odio saepe, consequatur quas?
+                  </div>
+                </div>
+                <div class="flex justify-end mb-4">
+                  <div>
+                    <div class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Magnam, repudiandae.
+                    </div>
+
+                    <div class="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Debitis, reiciendis!
+                    </div>
+                  </div>
+                 
+                </div>
+                <div class="flex justify-start mb-4">
+                  
+                  <div class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
+                    happy holiday guys!
+                  </div>
+                </div>
+              </div>
+              <div class="py-5">
+                <input
+                  class="w-full bg-gray-300 py-5 px-3 rounded-xl"
+                  type="text"
+                  placeholder="Type your message here..."
+                />
+              </div>
+            </div>
+            <div class="w-2/5 border-l-2 px-5">
+              <div class="flex flex-col">
+                <div class="font-semibold text-xl py-4">Mern Stack Group</div>
+                <img
+                  src="https://picsum.photos/200"
+                  class="object-cover rounded-xl h-64"
+                  alt=""
+                />
+                <div class="font-semibold py-4">Created 22 Sep 2021</div>
+                <div class="font-light">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Deserunt, perspiciatis!
+                </div>
+              </div>
             </div>
           </div>
         </div>
