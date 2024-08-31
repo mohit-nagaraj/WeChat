af2217e1853c7396ec5386883031e19042bc9f31
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-08-31 21:56:57 +0530

    docker files

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import authenticateToken from './utils/verify.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
